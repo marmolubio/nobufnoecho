72555a0ce6408255c9355ea6672393da2ccecb15
marmolubio <marmolubio7@hotmail.com>
2010-12-06 16:54:57 +0100

    first commit

---
+++ nobufnoecho.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import System.IO
+import Monad
+import Char
+
+main :: IO ()
+main = do 
+        hSetBuffering stdin NoBuffering
+        hSetBuffering stdout NoBuffering
+        hSetEcho stdin False 
+        putStrLn "Type 'e' to echo, 'q' to quit."
+        inputLoop []
+          
+inputLoop :: [Char] -> IO ()
+inputLoop s = do 
+        c <- getChar
+        case c of
+                'q' -> do
+                                putStrLn ""
+                                return ()
+                'e' -> do
+                                putStr (reverse s)
+                                inputLoop []
+                _   -> inputLoop (c:s)
